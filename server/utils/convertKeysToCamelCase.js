349ffc20ff3f84fe1478c7986e82dcdd0850fc04
Sayan Saha <sayansaha1082@gmail.com>
2021-05-25 13:30:13 -0700

    Refactor utils package structure

---
+++ server/utils/convertKeysToCamelCase.js
@@ -0,0 +1,28 @@
+// recursively converts the keys of an object from snake_case to camelCase
+function convertKeysToCamelCase(obj) {
+  const newObj = {};
+  if (obj === null || obj === undefined) {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    return obj.map((value) => convertKeysToCamelCase(value));
+  }
+  Object.entries(obj).forEach(([key, value]) => {
+    if (
+      Array.isArray(value) ||
+      (value !== null &&
+        typeof value === "object" &&
+        value.constructor === Object)
+    ) {
+      value = convertKeysToCamelCase(value);
+    }
+    let newKey = key.replace(/_id/g, "ID");
+    newKey = newKey.replace(/_\w/g, (match) =>
+      match.replace("_", "").toUpperCase()
+    );
+    newObj[newKey] = value;
+  });
+  return newObj;
+}
+
+module.exports = convertKeysToCamelCase;
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,29 +1,4 @@
-// recursively converts the keys of an object from snake_case to camelCase
-function convertKeysToCamelCase(obj) {
-  const newObj = {};
-  if (obj === null || obj === undefined) {
-    return obj;
-  }
-  if (Array.isArray(obj)) {
-    return obj.map((value) => convertKeysToCamelCase(value));
-  }
-  Object.entries(obj).forEach(([key, value]) => {
-    if (
-      Array.isArray(value) ||
-      (value !== null &&
-        typeof value === "object" &&
-        value.constructor === Object)
-    ) {
-      value = convertKeysToCamelCase(value);
-    }
-    let newKey = key.replace(/_id/g, "ID");
-    newKey = newKey.replace(/_\w/g, (match) =>
-      match.replace("_", "").toUpperCase()
-    );
-    newObj[newKey] = value;
-  });
-  return newObj;
-}
+const convertKeysToCamelCase = require("./convertKeysToCamelCase");
 
 module.exports = {
   convertKeysToCamelCase,
