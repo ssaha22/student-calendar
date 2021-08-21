3be06a1b38ea9ed9720f6dc275d09d8689ad52c4
Sayan Saha <sayansaha1082@gmail.com>
2021-08-20 21:34:06 -0700

    Setup redux state

---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  id: null,
+  authToken: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: initialState,
+  reducers: {
+    login: (_state, action) => action.payload,
+    logout: (_state) => initialState,
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export default userSlice.reducer;
