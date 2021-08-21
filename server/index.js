7af74f3a69dcb4e9413a40e8e1b4faede3722b8d
Sayan Saha <sayansaha1082@gmail.com>
2021-08-21 15:19:28 -0700

    Enable cors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+
 app.use(express.json());
+app.use(cors());
 
 const routes = require("./routes");
 app.use("/api/v1", routes);
 
-app.listen(process.env.PORT || 8000);
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
